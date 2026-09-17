21970cb50618dd970802cdbd74c5f0895747c7ac
José Soto <loeza.soto@gmail.com>
2026-09-17 12:23:36 -0700

    OS

--- script.js
+++ script.js
@@ -1,5 +1,57 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Seleccionamos todos los items que tienen un submenú
+    const userAgent = window.navigator.userAgent.toLowerCase()
+    const platform = window.navigator.platform.toLowerCase()
+    
+    const isMobile = /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent);
+
+    let os = 'windows';
+    if (userAgent.includes('mac') || platform.includes('mac')) {
+        os = 'macos';
+    } else if (userAgent.includes('linux') || platform.includes('linux')) {
+        os = 'linux';
+    } else if (userAgent.includes('bsd') || platform.includes('bsd')) {
+        os = 'bsd';
+    }
+
+    const reposSafewill = {
+        windows: "https://github.com/josesoto8/Safewill-Data-Windows",
+        macos: "https://github.com/josesoto8/Safewill-Data-MacOS",
+        linux: "https://github.com/josesoto8/Safewill-Data-Linux",
+        bsd: "https://github.com/josesoto8/Safewill-Data-BSD"
+    };
+
+    const reposVSData = {
+        windows: "https://github.com/josesoto8/VSData-Windows",
+        macos: "https://github.com/josesoto8/VSData-MacOS",
+        linux: "https://github.com/josesoto8/VSData-Linux",
+        bsd: "https://github.com/josesoto8/VSData-BSD"
+    };
+
+    const perfilGithub = "https://github.com/josesoto8"
+
+    const safewillGhLink = document.getElementById('safewill-github');
+    if (safewillGhLink) {
+        if (isMobile) {
+            safewillGhLink.href = perfilGithub;
+            safewillGhLink.querySelector('img').title = "Ver todos en mi perfil de GitHub";
+        } else if (reposSafewill[os]) {
+            safewillGhLink.href = reposSafewill[os];
+            safewillGhLink.querySelector('img').title = `Repositorio GitHub (${os.toUpperCase()})`;
+        }
+    }
+
+    const vsdataGhLink = document.getElementById('vsdata-github');
+    if (vsdataGhLink) {
+        if (isMobile) {
+            vsdataGhLink.href = perfilGithub;
+            vsdataGhLink.querySelector('img').title = "Ver todos en mi perfil de GitHub";
+        } else if (reposVSData[os]) {
+            vsdataGhLink.href = reposVSData[os];
+            vsdataGhLink.querySelector('img').title = `Repositorio GitHub (${os.toUpperCase()})`;
+        }
+    }
+    
     const menus = document.querySelectorAll('.has-submenu');
 
     menus.forEach(menu => {
